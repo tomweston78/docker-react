0dd826d4ae3595a8240985a9dcfa8ce01832d1f7
Tom Weston <tomweston78@yahoo.com>
2019-03-06 22:18:02 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there!
+            I was changed!
           </p>
           <a
             className="App-link"
